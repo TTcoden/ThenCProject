a2d2d983818768aa5c84f402878a2099cf39820e
TTcoden <consultoria786@gmail.com>
2024-04-05 14:02:45 -0300

    primeiro PUSH oficial:

--- ThenC/ThenC/obj/Release/net7.0/ThenC.AssemblyInfo.cs
+++ ThenC/ThenC/obj/Release/net7.0/ThenC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThenC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820ef5d3d651144baa1b799cca21e14081d49bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThenC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThenC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
