9e9c0706858e872540f745e3ecad97e2c8a24ea8
Romulo Breno <romulo.breno12@gmail.com>
2024-04-05 14:28:21 -0300

    Subida Person Model

--- ThenC/ThenC/Models/PersonModel.cs
+++ ThenC/ThenC/Models/PersonModel.cs
@@ -2,7 +2,7 @@
 {
     public class PersonModel
     {
-
+        //teste
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Address { get; set; }
