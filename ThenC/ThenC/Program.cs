0000d88ffcfc3ff3f3b2d77e320eccca717aeaf2
Romulo Breno <romulo.breno12@gmail.com>
2024-04-09 11:53:00 -0300

    Criação de home para inicio do projeto

--- ThenC/ThenC/Program.cs
+++ ThenC/ThenC/Program.cs
@@ -37,6 +37,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Person}/{action=Index}/{id?}");
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
